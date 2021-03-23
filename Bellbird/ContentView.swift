cac50b0751645894197a0cbed0eca2cbbbec6b83
Annie Wang <s028016@students.lmsd.org>
2021-03-23 11:23:24 -0400

    tried implementing old alarm framework from a previous app into the project, AVAudioPlayer seems to break the project strangely enough. Also added a sound to the project.

--- Bellbird/ContentView.swift
+++ Bellbird/ContentView.swift
@@ -5,15 +5,36 @@
 //  Created by Brandyn Cronin (student LM) on 3/11/21.
 //  Copyright © 2021 Brandyn Cronin (student LM). All rights reserved.
 //
-
+import AVFoundation
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+    //looks like the AVAudioPlayer is causing issues
+    
+    //@State private var player: AVAudioPlayer!
+    
+    //let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    //@State private var timeRemainingSeconds = 5
+    
+    //Leave date here as it might be useful in the future
+    //@State private var date = Date()
+    
+
+    //default sound for testing
+    //let Default = Bundle.main.path(forResource: "Call", ofType: "mp3")!
+    
+    
     var body: some View {
         VStack{
-            Text("Goodbye, World!")
-            Text("commit test")
-            Text("alarm branch commit test")
+            Text("hello")
+            //Text("alarm branch commit test")
+            //importing old framework over and building off of there
+            
+            //once timer runs down the sound plays and then it resets
+            
+            
             
         }
         
@@ -24,4 +45,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
+    }
