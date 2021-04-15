3d2e01429d38ff4e082b663373804fd6a59dcaba
Annie Wang <s028016@students.lmsd.org>
2021-04-15 14:27:30 -0400

    deleted the file and remade it as a view

--- Bellbird/SoundClass.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SoundClass.swift
-//  Bellbird
-//
-//  Created by Annie Wang (student LM) on 4/15/21.
-//  Copyright © 2021 Brandyn Cronin (student LM). All rights reserved.
-//
-
-import Foundation
---
+++ Bellbird/SoundView.swift
@@ -0,0 +1,21 @@
+//
+//  SoundView.swift
+//  Bellbird
+//
+//  Created by Annie Wang (student LM) on 4/15/21.
+//  Copyright © 2021 Brandyn Cronin (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct SoundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SoundView_Previews: PreviewProvider {
+    static var previews: some View {
+        SoundView()
+    }
+}
