8f651c6345dd03166c6fdbb5f5689bb78aa976ae
Annie Wang <s028016@students.lmsd.org>
2021-04-15 14:35:49 -0400

    to clarify, this new sound view is so that sound isn't on main

--- Bellbird/SoundView.swift
+++ Bellbird/SoundView.swift
@@ -5,12 +5,31 @@
 //  Created by Annie Wang (student LM) on 4/15/21.
 //  Copyright © 2021 Brandyn Cronin (student LM). All rights reserved.
 //
-
+import AVFoundation
 import SwiftUI
 
+//Sound View for the Default noise
 struct SoundView: View {
+    //plays the audio
+    @State private var player: AVAudioPlayer!
+    //need a variable for the time the alarm will sound
+    let blip = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    //testing timer before we do anything else 
+    @State private var timeRemainingSeconds = 5
+    //sound
+    let pitch = Bundle.main.path(forResource: "Call", ofType: "mp3")!
+    
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+            Text("\(timeRemainingSeconds)").onReceive(blip){value in
+            if(self.timeRemainingSeconds>0){
+                print("bruh moment")
+                self.timeRemainingSeconds-=1
+            }
+        
     }
 }
 
@@ -18,4 +37,5 @@ struct SoundView_Previews: PreviewProvider {
     static var previews: some View {
         SoundView()
     }
+}
 }
