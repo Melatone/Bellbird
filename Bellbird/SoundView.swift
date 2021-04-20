31525be45ca85aab3e38f2b1e4f1bbc6f4bdd9db
Annie Wang <s028016@students.lmsd.org>
2021-04-20 11:09:23 -0400

    moved the repeater alarm to a different view

--- Bellbird/SoundView.swift
+++ Bellbird/SoundView.swift
@@ -7,35 +7,121 @@
 //
 import AVFoundation
 import SwiftUI
-
+//Repeater View 
 //Sound View for the Default noise
 struct SoundView: View {
-    //plays the audio
-    @State private var player: AVAudioPlayer!
-    //need a variable for the time the alarm will sound
-    let blip = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    //testing timer before we do anything else 
-    @State private var timeRemainingSeconds = 5
-    //sound
-    let pitch = Bundle.main.path(forResource: "Call", ofType: "mp3")!
-    
-    
-    
-    
-    var body: some View {
-        
-            Text("\(timeRemainingSeconds)").onReceive(blip){value in
-            if(self.timeRemainingSeconds>0){
-                print("bruh moment")
-                self.timeRemainingSeconds-=1
-            }
-        
-    }
-}
+     @State private var player: AVAudioPlayer!
+       
+       @State var blip = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+       //adding a timer to test running a sound for a certain amount of time/repeating
+       
+       
+       
+       @State private var timeRemainingSeconds = 5
+       //added new thing to attempt to try and play sound for a minute
+       //Commented out for time being
+       //@State private var timeSoundPlay = 60
+       
+       
+       
+       
+       //Do we need date? Just leaving it here just in case its useful in the future
+       //@State private var date = Date()
+       
 
+       //default sound for testing
+       //so the first error here was that i had renamed the file so that it wasn't an mp3 anymore...whoops
+       //Don't do that
+       let pitch = Bundle.main.path(forResource: "Call", ofType: "mp3")!
+      
+       
+       
+       
+       
+       
+       var body: some View {
+           VStack{
+               Text("\(timeRemainingSeconds)").onReceive(blip){value in
+                   if(self.timeRemainingSeconds>0){
+                       print("bruh moment")
+                       self.timeRemainingSeconds-=1
+                   }else{
+                       self.blip.upstream.connect().cancel()
+                       //added sound to test repeating/playing a sound for a certain amount of time
+                       
+                       //play sound for a certain amount of time/repeat
+                       //ok i got errors with the old method I tried time to run another routine
+                       //use a wait timer is what stack overflow likes
+                       
+                       //the below code works commenting out for testing purposes
+                       
+                       
+                       
+                       do{let url = URL(fileURLWithPath: self.pitch)
+                           
+                           
+                           self.player = try AVAudioPlayer(contentsOf: url)
+                           
+                           //this works what is mainly the problem now is the timer bugging out
+                           let timeLimit = 20.0
+                           
+                           
+                               self.player.play()
+                           
+                           
+                           //hopefully this stops the sound from playing
+                           Timer.scheduledTimer(withTimeInterval: timeLimit, repeats: false) { (timer) in
+                               
+                              
+                               
+                               
+                               self.player.stop()
+                               
+                               timer.invalidate()
+                               //what is weird here is that it should just reset once but it is not doing that?
+                               
+                               //THIS IS SO HYPE IT WORKS NOW
+                           
+                               self.timeRemainingSeconds=5
+                               self.blip = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+                               //invalidate the timer after this and start over?
+                               
+                           }
+                           
+                           
+                           
+                          
+                           
+                           
+                           
+                           //self.player.stop()
+                               
+                               //self.timeSoundPlay-=1
+                           
+                           
+                           
+                           
+                           
+                       }catch{
+                           print("no")
+                           //catch is just in case
+                           
+                       }
+                       
+
+                       
+
+                           }
+                  
+                   
+                   
+               
 struct SoundView_Previews: PreviewProvider {
     static var previews: some View {
         SoundView()
     }
+}
+}
+}
 }
 }
