12717bfac28eca9fec10e990269874ad85936717
Annie Wang <s028016@students.lmsd.org>
2021-05-04 12:01:44 -0400

    cleaned up the comments

--- Bellbird/SoundView.swift
+++ Bellbird/SoundView.swift
@@ -24,46 +24,29 @@ struct SoundView: View {
      @State private var player: AVAudioPlayer!
    //Timer
        @State var blip = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-       //adding a timer to test running a sound for a certain amount of time/repeating
-    
+       
        
       //How long the sound plays/how long until the timer resets
     @State private var timeLimit = 20.0
     //How much time the timer spends to run down to zero
        @State private var timeRemainingSeconds = 5
-       //added new thing to attempt to try and play sound for a minute
-       //Commented out for time being
-       //@State private var timeSoundPlay = 60
        
        
        
        
-       //Do we need date? Just leaving it here just in case its useful in the future
-       //@State private var date = Date()
        
-
-       //default sound for testing
+      
+       
        //so the first error here was that i had renamed the file so that it wasn't an mp3 anymore...whoops
        //Don't do that
-    //need a way to change the sound
-    //make the name a variable? Somehow?
-    
-    //Could possibly change the method the sound is played? I don't understand it
-    //Cannot modify the variable so we have to pass it through a function to make it immutable
-    //This might also be useless but at least this is a whole different view from which code can be recycled
     
     //Sound the player plays
+    //Used to want to hard code to change sound but that is not needed
        let pitch = Bundle.main.path(forResource: "Call", ofType: "mp3")!
     
-    //Rob knows how to change between sounds so this has been deprecated
-    //change time interval i guess? that's easy
+    //Rob knows how to change between sounds so there was no need to
+    //hardcode to change sound
     
-    //basically im just gonna hardcode this in stupidly
-    
-    //let pitchTwo = Bundle.main.path(forResource: "Goose", ofType: "mp3")!
-    //let pitchThree = Bundle.main.path(forResource: "Robin", ofType: "mp3")!
-    
-      
        
        
        
@@ -92,42 +75,37 @@ struct SoundView: View {
                        self.blip.upstream.connect().cancel()
                        //added sound to test repeating/playing a sound for a certain amount of time
                        
-                       //play sound for a certain amount of time/repeat
-                       //ok i got errors with the old method I tried time to run another routine
-                       //use a wait timer is what stack overflow likes
-                       
-                       //the below code works commenting out for testing purposes
-                       
                        
                        
                        do{
                         
                         let url = URL(fileURLWithPath: self.pitch)
                            
-                           
+                           //loads up the sound
                            self.player = try AVAudioPlayer(contentsOf: url)
                            
-                           //this works what is mainly the problem now is the timer bugging out
-                           //let timeLimit = 20.0
+                          
                            
-                           
+                           //plays the sound
                                self.player.play()
                            
                            
                            //hopefully this stops the sound from playing
+                        //for 20 seconds the sound plays
                         Timer.scheduledTimer(withTimeInterval: self.timeLimit, repeats: false) { (timer) in
                                
                               
                                
-                               
+                               //after the 20 seconds the player stops
                                self.player.stop()
                                
                                
                               
-                           
+                           //the time resets
                                self.timeRemainingSeconds=5
+                            //the timer is revalidated
                                self.blip = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-                               //invalidate the timer after this and start over?
+                               
                                
                            }
                            
